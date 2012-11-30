f2300cfa8de0c6ef73853656dd2620257158ba8f
Jiri Sitina <jiri.sitina@gooddata.com>
2012-11-30 14:47:53 +0100

    Small update - IMAP is now able to create folder + read/write message inside easily

---
+++ src/main/java/net/sitina/sync/ImapFileSync.java
@@ -0,0 +1,15 @@
+package net.sitina.sync;
+
+public class ImapFileSync {
+
+    public static void main( String[] args ) {
+
+        while (true) {
+            // read IMAP folder and look for files
+            // check that are those files in sync (eg. look at the files, load their meta and copy newer version from/to server depending on the latest date); create missing files
+
+            // wait for a while
+        }
+
+    }
+}
--- src/main/java/net/sitina/sync/InboxReader.java
+++ src/main/java/net/sitina/sync/InboxReader.java
@@ -1,46 +1,67 @@
 package net.sitina.sync;
 
-import com.sun.mail.imap.IMAPMessage;
-import com.sun.xml.internal.org.jvnet.mimepull.MIMEMessage;
-
 import java.util.Properties;
 
-import javax.mail.*;
+import javax.mail.Address;
+import javax.mail.Flags.Flag;
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.NoSuchProviderException;
+import javax.mail.Session;
+import javax.mail.Store;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 public class InboxReader {
 
     public static void main(String args[]) {
         Properties props = System.getProperties();
         props.setProperty("mail.store.protocol", "imaps");
+
         try {
             Session session = Session.getDefaultInstance(props, null);
             Store store = session.getStore("imaps");
             store.connect("imap.gmail.com", "jirka.sitina@gmail.com", "*****");
-            System.out.println(store);
 
             Folder inbox = store.getFolder("Inbox");
-
             inbox = store.getFolder("Notes");
 
-            Message m = new MIMEMessage(session);
-
-            Address a = new InternetAddress("a@a.com", "A a");
-            Address b = new InternetAddress("fake@java2s.com");
-
-            m.setContent("Mail content", "text/plain");
-            m.setFrom(a);
-            m.setRecipient(Message.RecipientType.TO, b);
-            m.setSubject("subject");
-
-            inbox.appendMessages(new Message[]{m});
-
-            inbox.open(Folder.READ_ONLY);
-            Message messages[] = inbox.getMessages();
-            for(Message message:messages) {
-                InternetAddress from = (InternetAddress)message.getFrom()[0];
-                System.out.println(from.getAddress());
+            Folder folder = store.getDefaultFolder().getFolder("IMAP File Sync");
+            if (!folder.exists()) {
+                folder.create(Folder.HOLDS_MESSAGES);
             }
+            folder.open(Folder.HOLDS_MESSAGES);
+
+            Message message = new MimeMessage(session);
+
+            Address from = new InternetAddress("jirka.sitina@gmail.com");
+            Address to = new InternetAddress("jirka.sitina@gmail.com");
+
+            message.setContent("Content of the note", "text/plain");
+            message.setFrom(from);
+            message.setRecipient(Message.RecipientType.TO, to);
+            message.setSubject("/Users/jirka/Documents/workspace/imap-file-sync/tmp.txt");
+
+            inbox.appendMessages(new Message[] { message });
+            inbox.open(Folder.READ_WRITE);
+
+            // inbox.open(Folder.READ_ONLY);
+            Message messages[] = inbox.getMessages();
+            for (Message m : messages) {
+                InternetAddress fromAddress = (InternetAddress) m.getFrom()[0];
+                System.out.println(fromAddress.getAddress());
+
+                if (fromAddress.getAddress().toString().equals("jirka.sitina@gmail.com")) {
+                    m.setFlag(Flag.SEEN, true);
+                    //                    Flags flags = new Flags(Flag.DRAFT);
+                    //                    boolean arg2 = true;
+                    //                    inbox.setFlags(new Message[] { m }, flags, arg2);
+                }
+
+            }
+
+            folder.appendMessages(messages);
         } catch (NoSuchProviderException e) {
             e.printStackTrace();
             System.exit(1);
